179092504b729890c59d67555a1c119833b084cd
michaelfitra <fitra.michael@gmail.com>
2025-07-27 22:11:02 +0700

    Clean up order table layout

--- views/admin/order.php
+++ views/admin/order.php
@@ -7,6 +7,7 @@
     <title>Order - Kedai Barokah</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../../assets/css/admin.css">
 
 </head>
@@ -19,22 +20,6 @@
 
         <!-- Main Content -->
         <div class="col-md-10">
-            <!-- Topbar -->
-            <!-- <div class="topbar">
-                <div class="dropdown">
-                    <a href="#" class="dropdown-toggle text-dark text-decoration-none" id="adminDropdown"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi bi-person-fill"></i> Admin
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="adminDropdown">
-                        <li><a class="dropdown-item" href="#">Laporkan Masalah</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item text-danger" href="#">Logout</a></li>
-                    </ul>
-                </div>
-            </div> -->
 
             <!-- Page Content -->
             <div class="main-content">
@@ -78,9 +63,10 @@
                                 <th>Nama</th>
                                 <th>Tanggal</th>
                                 <th>Total</th>
-                                <th>Status Pembayaran</th>
-                                <th>Metode Ambil</th>
+                                <!-- <th>Status Pembayaran</th> -->
+                                <!-- <th>Metode Ambil</th> -->
                                 <th>Aksi</th>
+                                <th>Cetak Struk</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -88,31 +74,37 @@
                                 <td>1</td>
                                 <td>001</td>
                                 <td>Andi</td>
-                                <td>28 Mei 2025<br>10:00</td>
+                                <td>28 Mei 2025, 10:00</td>
                                 <td>Rp 100.000</td>
-                                <td>Sudah Dibayar</td>
-                                <td>Ambil di Toko</td>
-                                <td><span class="badge-selesai">Selesai</span></td>
+                                <!-- <td>Sudah Dibayar</td> -->
+                                <!-- <td>Ambil di Toko</td> -->
+                                <td><span class="badge-selesai">Selesai</span>&nbsp;&nbsp;&nbsp;<i
+                                        class="fa-solid fa-pen-to-square"></i></td>
+                                <td><i class="fa-solid fa-print"></i></td>
                             </tr>
                             <tr>
                                 <td>2</td>
                                 <td>002</td>
                                 <td>Andi</td>
-                                <td>28 Mei 2025<br>10:00</td>
+                                <td>28 Mei 2025, 10:00</td>
                                 <td>Rp 100.000</td>
-                                <td>Sudah Dibayar</td>
-                                <td>Pesan Kurir Sendiri</td>
-                                <td><span class="badge-selesai">Selesai</span></td>
+                                <!-- <td>Sudah Dibayar</td> -->
+                                <!-- <td>Pesan Kurir Sendiri</td> -->
+                                <td><span class="badge-selesai">Selesai</span>&nbsp;&nbsp;&nbsp;<i
+                                        class="fa-solid fa-pen-to-square"></i></td>
+                                <td><i class="fa-solid fa-print"></i></td>
                             </tr>
                             <tr>
                                 <td>3</td>
                                 <td>003</td>
                                 <td>Andi</td>
-                                <td>28 Mei 2025<br>10:00</td>
+                                <td>28 Mei 2025, 10:00</td>
                                 <td>Rp 100.000</td>
-                                <td>Sudah Dibayar</td>
-                                <td>Pesan Kurir Sendiri</td>
-                                <td><span class="badge-selesai">Selesai</span></td>
+                                <!-- <td>Sudah Dibayar</td> -->
+                                <!-- <td>Pesan Kurir Sendiri</td> -->
+                                <td><span class="badge-selesai">Selesai</span>&nbsp;&nbsp;&nbsp;<i
+                                        class="fa-solid fa-pen-to-square"></i></td>
+                                <td><i class="fa-solid fa-print"></i></td>
                             </tr>
                         </tbody>
                     </table>
